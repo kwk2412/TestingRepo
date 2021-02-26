00db61ec9e4a5da4f709dc3cc4a260640e45cf4f
kwk2412 <kwk2412@email.vccs.edu>
2021-02-25 20:55:27 -0500

    Commit 1 to BranchX

---
+++ src/githubTesting/GitHubTesting2.java
@@ -0,0 +1,8 @@
+package githubTesting;
+
+public class GitHubTesting2 {
+	
+	public static void printMessage() {
+		System.out.println("This is a message in the second test class of the GitHubTesting project.");
+	}
+}
